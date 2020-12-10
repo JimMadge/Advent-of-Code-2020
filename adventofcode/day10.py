dd9f4e0633bd10c926c5051e4a1375eafc85f7dc
Jim Madge <jmmadge@gmail.com>
2020-12-10 11:58:50 +0000

    Tidy day 10

--- adventofcode/day10.py
+++ adventofcode/day10.py
@@ -3,11 +3,8 @@ from functools import cache
 
 
 def pick_order(adapters):
-    # Add outlet
-    adapters = adapters[:] + [0]
-    # Add device
-    adapters += [max(adapters) + 3]
-    return sorted(adapters)
+    # Add outlet and device
+    return sorted(adapters[:] + [0] + [max(adapters) + 3])
 
 
 def count_jumps(adapters):
@@ -27,17 +24,17 @@ def jump_product(adapters):
 
 
 def count_routes(adapters):
-    adapters = pick_order(adapters)
+    adapters = [0] + sorted(adapters)
 
     @cache
     def routes(index):
+        # If we have reached the end this is the only route
         if index == len(adapters) - 1:
             return 1
 
-        count = 0
-        for i in range(index+1, min(index+4, len(adapters))):
-            if adapters[i] - adapters[index] <= 3:
-                count += routes(i)
-        return count
+        return sum(
+            routes(i) for i in range(index+1, min(index+4, len(adapters)))
+            if adapters[i] - adapters[index] <= 3
+        )
 
     return routes(0)
