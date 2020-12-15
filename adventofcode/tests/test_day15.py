857af803d3246f684c0c200c5c00613cd6d82d3b
Jim Madge <jmmadge@gmail.com>
2020-12-15 14:57:25 +0000

    Skip slow tests

--- adventofcode/tests/test_day15.py
+++ adventofcode/tests/test_day15.py
@@ -13,13 +13,13 @@ import pytest
         ([2, 3, 1], None, 78),
         ([3, 2, 1], None, 438),
         ([3, 1, 2], None, 1836),
-        ([0, 3, 6], 30_000_000, 175594),
-        ([1, 3, 2], 30_000_000, 2578),
-        ([2, 1, 3], 30_000_000, 3544142),
-        ([1, 2, 3], 30_000_000, 261214),
-        ([2, 3, 1], 30_000_000, 6895259),
-        ([3, 2, 1], 30_000_000, 18),
-        ([3, 1, 2], 30_000_000, 362),
+        pytest.param([0, 3, 6], 30_000_000, 175594, marks=pytest.mark.skip),
+        pytest.param([1, 3, 2], 30_000_000, 2578, marks=pytest.mark.skip),
+        pytest.param([2, 1, 3], 30_000_000, 3544142, marks=pytest.mark.skip),
+        pytest.param([1, 2, 3], 30_000_000, 261214, marks=pytest.mark.skip),
+        pytest.param([2, 3, 1], 30_000_000, 6895259, marks=pytest.mark.skip),
+        pytest.param([3, 2, 1], 30_000_000, 18, marks=pytest.mark.skip),
+        pytest.param([3, 1, 2], 30_000_000, 362, marks=pytest.mark.skip),
     ]
 )
 def test_memory_game(starting_numbers, rounds, expected):
