307eb2ab29a9d213fd850c7babb3a4e6c5dec685
Jim Madge <jmmadge@gmail.com>
2020-12-15 11:45:02 +0000

    Improve day 15 solution

--- adventofcode/day15.py
+++ adventofcode/day15.py
@@ -3,33 +3,22 @@ def memory_game(starting_numbers, end_turn=2020):
     # time they have been said
     numbers = dict()
 
-    # Initialise using starting numbers
-    numbers = {number: ([turn], True)
-               for turn, number in enumerate(starting_numbers, start=1)}
+    # Initialise using starting numbers except for the last
+    numbers = {number: turn
+               for turn, number in enumerate(starting_numbers[:-1], start=1)}
 
     # Play the game
     last_number = starting_numbers[-1]
-    for turn in range(len(numbers)+1, end_turn+1):
-        turns_said, first_occurance = numbers[last_number]
-
-        if first_occurance:
-            # If this is the first occurrence, say 0
+    for turn in range(len(starting_numbers)+1, end_turn+1):
+        if last_number in numbers.keys():
+            # Current number is difference between last turn and previous time
+            # last number was said
+            current_number = (turn - 1) - numbers[last_number]
+        else:
+            # First time number was said, current number is 0
             current_number = 0
-        else:
-            # Otherwise say the difference between the last turn and the time
-            # the number was previously said (not including the last turn!)
-            # current_number = turns_said[-1] - turns_said[-2]
-            current_number = (turn-1) - turns_said[-2]
 
-        if current_number in numbers.keys():
-            # Update an existing occurrence
-            numbers[current_number] = (numbers[current_number][0][-1:]+[turn],
-                                       False)
-        else:
-            # Add a new occurrence to the record
-            numbers[current_number] = ([turn], True)
-
-        # print(turn, current_number)
+        numbers[last_number] = turn - 1
         last_number = current_number
 
     return current_number
